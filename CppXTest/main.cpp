e2b9f217c9aaddd77df1e89e9b3c90966d353e32
MatchX <fcs12@qq.com>
2016-10-12 22:19:47 +0800

    first pull

--- CppXTest/main.cpp
+++ CppXTest/main.cpp
@@ -8,72 +8,23 @@
 * This software is provided "as is" without express or implied
 * warranty, and with no claim as to its suitability for any purpose.
 */
-#include <tuple>
+#include <forward_list>
 #include <iostream>
-#include <complex>
-#include <string>
-
 using namespace std;
-
-// helper: print elements with index IDX and higher of tuple t having MAX elements
-template <int IDX, int MAX, typename... Args>
-struct PRINT_TUPLE
-{
-	static void print(std::ostream& strm, const std::tuple<Args...>& t)
-	{
-		strm << std::get<IDX>(t) << (IDX + 1 == MAX ? "" : ",");
-		PRINT_TUPLE<IDX + 1, MAX, Args...>::print(strm, t);
-	}
-};
-
-// partial specialization to end the recursion
-template <int MAX, typename... Args>
-struct PRINT_TUPLE<MAX, MAX, Args...>
-{
-	static void print(std::ostream& strm, const std::tuple<Args...>& t)
-	{
-	}
-};
-
-// output operator for tuples
-template <typename... Args>
-std::ostream& operator << (std::ostream& strm, const std::tuple<Args...>& t)
-{
-	strm << "[";
-	PRINT_TUPLE<0, sizeof...(Args), Args...>::print(strm, t);
-	return strm << "]";
-}
 
 int main()
 {
-	// create a four-element tuple
-	// - elements are initialized with default value (0 for fundamental types)
-// 	tuple<string, int, int, complex<double>> t;
-// 
-// 	// create and initialize a tuple explicitly
-// 	tuple<int, float, string> t1(41, 6.3, "nico");
-// 
-// 	// "iterate" over elements:
-// 	cout << get<0>(t1) << " ";
-// 	cout << get<1>(t1) << " ";
-// 	cout << get<2>(t1) << " ";
-// 	cout << endl;
-// 
-// 	// create tuple with make_tuple()
-// 	// - auto declares t2 with type of right-hand side
-// 	// - thus, type of t2 is tuple
-// 	auto t2 = make_tuple(22, 44, "nico");
-// 
-// 	// assign second value in t2 to t1
-// 	get<1>(t1) = get<1>(t2);
-// 
-// 	// comparison and assignment
-// 	// - including type conversion from tuple<int,int,const char*>
-// 	//   to tuple<int,float,string>
-// 	if (t1 < t2) {  // compares value for value
-// 		t1 = t2;    // OK, assigns value for value
-// 	}
+	// create forward-list container for some prime numbers
+	forward_list<long> coll = { 2, 3, 5, 7, 11, 13, 17 };
 
-	tuple <int, float, string> t(77, 1.1, "more light");
-	cout << "io: " << t << endl;
+	// resize two times
+	// - note: poor performance
+	coll.resize(9);
+	coll.resize(10, 99);
+
+	// print all elements:
+	for (auto elem : coll) {
+		cout << elem << ' ';
+	}
+	cout << endl;
 }
